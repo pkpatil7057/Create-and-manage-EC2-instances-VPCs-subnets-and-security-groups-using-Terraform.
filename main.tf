d7fcb51a181511be0767db2993db16ed2f9bad3e
pkpatil7057 <pkpatil7057@gmail.com>
2024-10-30 10:56:33 +0530

    Create-and-manage-EC2-instances-VPCs-subnets-and-security-groups-using-Terraform

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro
+  subnet_id     = aws_subnet.main_subnet.id
+  security_groups = [aws_security_group.allow_ssh.name]
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
